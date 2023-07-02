c9b5c9da43bff7d5efa019109039f0ab936dff64
Daniels Hogans <daniel.santos.ap@gmail.com>
2023-07-01 21:21:40 -0300

    usuarios table add

--- database/migrations/2023_07_02_000748_create_usuarios_table.php
+++ database/migrations/2023_07_02_000748_create_usuarios_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('USER', 30)->unique('user_key');
+            $table->string('NOME', 100);
+            $table->mediumText('PASSKEY');
+            $table->bollean('ACTIVE')->default(0);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('usuarios');
     }
 };
+
--- resources/views/AdminLTE320/login.blade.php
+++ resources/views/AdminLTE320/login.blade.php
@@ -1,26 +1,27 @@
 @extends('AdminLTE320.model')
 @section('body')
-<div class="hold-transition login-page">
-    <div class="login-box">
+<!-- <div class="hold-transition login-page"> -->
+<div class="hold-transition text-center" style="-webkit-align-items:center;-ms-flex-align:center;align-items:center;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;height:100vh;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center">
+    <div class="login-box hold-transition">
       <div class="login-logo">
         <a href="/"><b>{{env('APP_NAME')}}</b></a>
       </div>
       <!-- /.login-logo -->
       <div class="card">
         <div class="card-body login-card-body">
-          <p class="login-box-msg">Sign in to start your session</p>
+          <p class="login-box-msg">Inicie sua Sessão</p>
     
           <form action="../../index3.html" method="post">
             <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="Email">
+              <input type="email" class="form-control" placeholder="Usuário">
               <div class="input-group-append">
                 <div class="input-group-text">
-                  <span class="fas fa-envelope"></span>
+                  <span class="fas fa-user"></span>
                 </div>
               </div>
             </div>
             <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="Password">
+              <input type="password" class="form-control" placeholder="Senha">
               <div class="input-group-append">
                 <div class="input-group-text">
                   <span class="fas fa-lock"></span>
@@ -32,34 +33,20 @@
                 <div class="icheck-primary">
                   <input type="checkbox" id="remember">
                   <label for="remember">
-                    Remember Me
+                    Lembrar-me
                   </label>
                 </div>
               </div>
               <!-- /.col -->
               <div class="col-4">
-                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                <button type="submit" class="btn btn-primary btn-block">Entrar</button>
               </div>
               <!-- /.col -->
             </div>
           </form>
     
-          <div class="social-auth-links text-center mb-3">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-primary">
-              <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" class="btn btn-block btn-danger">
-              <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-            </a>
-          </div>
-          <!-- /.social-auth-links -->
-    
           <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-          </p>
-          <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
+            <a href="forgot-password.html">ESqueceu a senha?</a>
           </p>
         </div>
         <!-- /.login-card-body -->
